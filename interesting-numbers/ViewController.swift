0c9c627a6a890a39275d5c6c7c5c8181a57adf22
Anisovich_Maria <anisovich.masha@gmail.com>
2025-01-08 10:49:06 +0100

    Initial Commit

---
+++ interesting-numbers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  interesting-numbers
+//
+//  Created by Мария Анисович on 08.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
